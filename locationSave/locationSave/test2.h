d6eaebaa466200b5449f9b2991d8a6a24b199460
mac <376176293@qq.com>
2020-01-21 18:34:23 +0800

    no message

---
+++ locationSave/locationSave/test2.h
@@ -0,0 +1,17 @@
+//
+//  test2.h
+//  locationSave
+//
+//  Created by mac on 2020/1/21.
+//  Copyright © 2020 mac. All rights reserved.
+//
+
+#import "baseModel.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface test2 : baseModel
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ locationSave/locationSave/test2.m
@@ -0,0 +1,13 @@
+//
+//  test2.m
+//  locationSave
+//
+//  Created by mac on 2020/1/21.
+//  Copyright © 2020 mac. All rights reserved.
+//
+
+#import "test2.h"
+
+@implementation test2
+
+@end
